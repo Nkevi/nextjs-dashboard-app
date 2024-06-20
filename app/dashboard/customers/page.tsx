a96c49983e5d6cc58ed7193c9a509a5ddcf1d857
Kevin· <kevinnl5432@gmail.com>
2024-06-20 23:26:02 +0800

    add authentication and metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
 import { Card } from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
   return <p>customer page</p>;
 }
